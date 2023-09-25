06972a9b2a489618f4b02b75966aa1697bab2193
Joshua <hola@joshua.cl>
2023-09-25 17:08:04 -0300

    Configurando AuthSlice

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// Estado inicial de la app
+const initialState = {
+  status: 'checking', // 'not-authenticated', 'authenticated'
+  uid: null,
+  email: null,
+  displayName: null,
+  photoUrl: null,
+  errorMessage: null
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: ( state, action ) => {},
+    logout: ( state, payload ) => {},
+    checkingCredentials: ( state ) => {},
+  }
+});
+
+export const { login, logout, checkingCredentials } = authSlice.actions;
