9b4435765d17ec855fd752cfe6e1d15dc992072d
Joshua <hola@joshua.cl>
2023-11-02 17:15:16 -0300

    Creando boton/input para seleccionar multiples archivos

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { SaveOutlined } from '@mui/icons-material';
-import { Button, Grid, TextField, Typography } from '@mui/material';
+import { SaveOutlined, UploadOutlined } from '@mui/icons-material';
+import { Button, Grid, IconButton, TextField, Typography } from '@mui/material';
 import { ImageGallery } from '../components';
 
 import Swal from 'sweetalert2';
@@ -24,6 +24,9 @@ export const NoteView = () => {
     return formatDate.charAt(0).toUpperCase() + formatDate.slice(1);
   }, [ date ])
 
+  // Uso el hook "useRef" para simular un click, basicamente necesito "disparar" un input file que tengo oculto, se ha ocultado ya que la naturaleza del mismo no lo permite estilizar, entonces uso un componente que pueda estilizar, en este caso un boton de MaterialUi, para acceder a la funcionalidad del input file oculto
+  const fileInputRef = useRef();
+
   // Cada vez que el "formState" se actualice se va a despachar "setear" la nueva nota de manera "activa", en otras palabras: cada vez que la nota activa "cambie" (se edite el contenido de la nota) sera actualizada su información en el store
   useEffect(() => {
     dispatch( setActiveNote( formState ) );
@@ -42,6 +45,15 @@ export const NoteView = () => {
     dispatch( startSaveNote() );
   }
 
+  const onFileInputChange = ({ target }) => {
+    console.log( target.files );
+
+    if ( target.files === 0 ) return;
+
+    console.log('subiendo archivos');
+    // dispatch( startUploadingFiles( target.files ) );
+  }
+
   return (
     <Grid
     // className='animate__animated animate__fadeIn animate__slower'
@@ -52,6 +64,23 @@ export const NoteView = () => {
       </Grid>
 
       <Grid item>
+
+        <input
+          type="file"
+          multiple
+          ref={ fileInputRef }
+          onChange={ onFileInputChange }
+          style={{ display: 'none' }}
+        />
+
+        <IconButton
+          color='primary'
+          disabled={ isSaving }
+          onClick={ () => fileInputRef.current.click() }
+        >
+          <UploadOutlined />
+        </IconButton>
+
         <Button
           disabled={ isSaving }
           onClick={ onSaveNote }
