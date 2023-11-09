63c0b215d0f768518053efe8dab204abbafe08a8
Joshua <hola@joshua.cl>
2023-11-09 14:00:42 -0300

    Test authThunks checkingCredentials

---
+++ tests/store/auth/thunks.test.js
@@ -0,0 +1,19 @@
+import { checkingAuthentication, checkingCredentials } from "../../../src/store/auth";
+
+jest.mock('../../../src/firebase/providers');
+
+describe('Pruebas en AuthThunks', () => {
+
+  const dispatch = jest.fn();
+
+  beforeEach( () => jest.clearAllMocks() );
+
+  test('debe de invocar el checkingCredentials', async () => {
+
+    await checkingAuthentication()( dispatch );
+
+    expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() );
+
+  })
+
+});
