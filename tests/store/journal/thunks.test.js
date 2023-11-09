03752012234930105d602fdcf3a6710167f31210
Joshua <hola@joshua.cl>
2023-11-09 19:36:52 -0300

    Test journalThunks

--- tests/store/journal/thunks.test.js
+++ tests/store/journal/thunks.test.js
@@ -1,4 +1,6 @@
-import { startNewNote } from "../../../src/store/journal";
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import { addNewEmptyNote, savingNewNote, setActiveNote, startNewNote } from "../../../src/store/journal";
+import { FirebaseDB } from "../../../src/firebase/config";
 
 describe('Pruebas en Journal Thunks', () => {
 
@@ -15,8 +17,35 @@ describe('Pruebas en Journal Thunks', () => {
 
     await startNewNote()( dispatch, getState );
 
+    expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
 
+    expect( dispatch ).toHaveBeenCalledWith( addNewEmptyNote({
+      body: '',
+      title: '',
+      id: expect.any( String ),
+      date: expect.any( Number ),
+      imageUrls: []
+    }));
 
-  })
+    expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
+      body: '',
+      title: '',
+      id: expect.any( String ),
+      date: expect.any( Number ),
+      imageUrls: []
+    }));
+
+    // Borrar de Firebase
+    const collectionRef = collection( FirebaseDB, `${ uid }/journal/notes` );
+
+    const docs = await getDocs( collectionRef );
+
+    const deletePromises = [];
+
+    docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ) );
+
+    await Promise.all( deletePromises );
+
+  }, 10000)
 
 });
